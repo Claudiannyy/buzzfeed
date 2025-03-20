26fcae55126b5b6d754ea07876b9a31615c55d47
Claudianny <c_leticiatrajano@hotmail.com>
2025-03-20 17:52:49 -0300

    Inclusão de propriedades lógicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
 
 export const routes: Routes = [
    { path:"home",
-    component: HomeComponent}
+    component: HomeComponent},
+    { path: 'quizz', component: QuizzComponent }
 ];
-
--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 @Component({
   selector: 'app-quizz',
   imports: [],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
-  imageUrl= 'assets/images/logo.png';
+export class QuizzComponent implements OnInit{
+  title: string = ""
+  questions:any
+  questionSelected:any
+
+  answers: string []=[]
+  answerSelected:string=""
+
+  questionIndex:number =0
+  questionMaxIndex:number = 0
+
+  finished: boolean = false;
+  constructor(){}
+  ngOnInit():void{}
+
 }
